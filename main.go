581dc65851d295863c24a4c3e0d648945b1cf03b
muzewei1 <139840179+muzewei1@users.noreply.github.com>
2023-09-16 16:16:15 +0800

    Update main.go

--- main.go
+++ main.go
@@ -225,7 +225,7 @@ func init() {
 	// 直接写死 URL 时，请更改下面第二个参数
 	url := flag.String("u", "ws://127.0.0.1:6700", "Set Url of WSClient.")
 	// 默认昵称
-	adana := flag.String("n", "椛椛", "Set default nickname.")
+	adana := flag.String("n", "白白", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
 	runcfg := flag.String("c", "", "Run from config file.")
 	save := flag.String("s", "", "Save default config to file and exit.")
@@ -257,8 +257,10 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	sus = append(sus, 3229273932)
+	sus = append(sus, 279199038)
+	sus = append(sus, 3013627990)
+	sus = append(sus, 3463529674)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
@@ -286,7 +288,7 @@ func init() {
 	}
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:       append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
+		NickName:       append([]string{*adana}, "白白", "bb", "lingdong", "秋白"),
 		CommandPrefix:  *prefix,
 		SuperUsers:     sus,
 		RingLen:        *rsz,
